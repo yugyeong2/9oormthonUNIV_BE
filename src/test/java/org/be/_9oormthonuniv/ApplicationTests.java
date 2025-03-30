63bdf619adf103e6b1886e17278156e71a885f55
Yugyeong Park <bonbonly7@naver.com>
2025-03-30 23:30:09 +0900

    9oormthonUNIV 백엔드 고급반 스터디 자료 init

---
+++ src/test/java/org/be/_9oormthonuniv/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.be._9oormthonuniv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
